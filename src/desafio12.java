3f51d4521e6b49d51211ab6fd243fbcc15b56a71
julianadev <designsilvaport@yahoo.com>
2025-05-28 17:27:09 -0300

    desafio distancia

---
+++ src/desafio12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class desafio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int km = sc.nextInt();
+        int tempoNecessario = 2 * km;
+        System.out.println(tempoNecessario + " minutos");
+    }
+}
