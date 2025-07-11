83411532be507fabfc2ffef8a8c66e8a387d6447
julianadev <designsilvaport@yahoo.com>
2025-07-10 21:38:25 -0300

    calculando distancia

--- src/desafio12.java
+++ src/desafio12.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class desafio12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // calculando distancia
         int km = sc.nextInt();
         int tempoNecessario = 2 * km;
         System.out.println(tempoNecessario + " minutos");
